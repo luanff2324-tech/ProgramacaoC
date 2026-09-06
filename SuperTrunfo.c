63dcf83a6ef8e3d43aedcb1f039fbd71b1d8642e
luanff2324-tech <luanff2324@gmail.com>
2026-09-06 17:03:38 +0000

    usei acarta 2

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -12,6 +12,8 @@ int pontosturisticos;
 int main() {
 
  struct  carta carta1;
+ struct  carta carta2;
+ 
 
  printf("Digite o nome da cidade: \n");
  scanf("%s", carta1.nome);
@@ -28,12 +30,34 @@ int main() {
  printf("Digite o numero turisticos da cidade: \n");
  scanf("%d",&carta1.pontosturisticos);
 
+ printf("Digite o nome da cidade: \n");
+ scanf("%s", carta2.nome);
+
+ printf("Digite o PIB da cidade: \n");
+ scanf("%f", &carta2.pib);
+
+ printf("Digite a população da cidade: \n");
+ scanf("%d",&carta2.população);
+
+ printf("Digite a area da cidade: \n");
+ scanf("%f", &carta2.area);
+
+ printf("Digite o numero turisticos da cidade: \n");
+ scanf("%d",&carta2.pontosturisticos);
+
  printf("\n---- carta----\n");
  printf("cidade:%s\n",carta1.nome);
  printf("PIB:%.2f\n",carta1.pib);
  printf("população:%d\n",carta1.população);
  printf("area:%.2f\n",carta1.area);
  printf("pontosturisticos:%d\n",carta1.pontosturisticos);
+
+ printf("\n---- carta----\n");
+ printf("cidade:%s\n",carta2.nome);
+ printf("PIB:%.2f\n",carta2.pib);
+ printf("população:%d\n",carta2.população);
+ printf("area:%.2f\n",carta2.area);
+ printf("pontosturisticos:%d\n",carta2.pontosturisticos);
 
  return 0;
 }
